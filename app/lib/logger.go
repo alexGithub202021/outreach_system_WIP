bd08f51af2f0327a89d9a7085678c8b6f18ce26e
work  2 <admin2@Alexs-MacBook-Air.local>
2026-07-15 19:14:30 +0700

    update main

--- app/lib/logger.go
+++ app/lib/logger.go
@@ -1,4 +1,4 @@
-package slog
+package lib
 
 import (
 	"io"
--- app/main.go
+++ app/main.go
@@ -3,19 +3,17 @@ package main
 import (
 	"fmt"
 	"log/slog"
-	logger "myapp/lib"
+	lib "myapp/lib"
 	"time"
 )
 
 var operation = "aggTrade"
 var url = fmt.Sprintf("wss://stream.binance.com:9443/ws/btcusdt@%s", operation)
-
-// var logPath = "app/logs/ws_conn_log.json"
 var logPath = "app/logs/logs.json"
 
 func main() {
 
-	cleanup, err := logger.InitGlobalLogger(logPath)
+	cleanup, err := lib.InitGlobalLogger(logPath)
 	if nil != err {
 		panic("Failed to initialize logger: " + err.Error())
 	}
@@ -26,22 +24,6 @@ func main() {
 	testSleep(1800)
 
 }
-
-// func getLogger(logPath string) (*slog.Logger, func(), error) {
-// 	file, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-// 	if err != nil {
-// 		return nil, nil, err
-// 	}
-
-// 	// Pass the file to the JSON handler
-// 	logger := slog.New(slog.NewJSONHandler(file, nil))
-
-// 	cleanup := func() {
-// 		file.Close()
-// 	}
-
-// 	return logger, cleanup, nil
-// }
 
 func testSleep(dur int) {
 
